c64176def8f364886e70e78b140d40fc769177a9
Usama Ahsan <usama8800@gmail.com>
2019-01-31 12:53:22 -0500

    Made setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+cd ~
+ln -s ~/Documents/dotfiles/.vim .vim
+ln -s ~/Documents/dotfiles/.vimrc .vimrc
+ln -s ~/Documents/dotfiles/.bash_profile .bash_profile
+ln -s ~/Documents/dotfiles/.bashrc .bashrc
